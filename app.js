e039e4377e19a73c044ada0b5fcd6d6bee475a8c
avlisesac <casejsilva@gmail.com>
2025-12-10 21:44:15 -0500

    add new user record if requirements are met

--- app.js
+++ app.js
@@ -1,21 +1,76 @@
-require('dotenv').config();
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var bcrypt = require("bcryptjs");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const db = require("./db");
+const { usersTable } = require("./src/db/schema");
+const { eq } = require("drizzle-orm");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/api', indexRouter);
-app.use('/api/users', usersRouter);
+app.use("/api", indexRouter);
+app.use("/api/users", usersRouter);
+
+app.post("/api/register", async (req, res, next) => {
+  console.log("req.body:", req.body);
+  try {
+    const { email, password } = req.body;
+    const missingRequiredParametersMessage = "Missing required parameters:";
+    const missingRequiredParameters = [];
+    if (!email) {
+      missingRequiredParameters.push("email");
+    }
+    if (!password) {
+      missingRequiredParameters.push("password");
+    }
+    if (missingRequiredParameters.length > 0) {
+      missingRequiredParametersMessage +=
+        missingRequiredParameters.concat(", ");
+      throw missingRequiredParametersMessage;
+    }
+
+    // Check for existing user w/ email.
+    const existingUsersWithEmail = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.email, email));
+    console.log("existingUsersWithEmail:", existingUsersWithEmail);
+
+    if (existingUsersWithEmail && existingUsersWithEmail.length > 0) {
+      throw "User with this email already exists.";
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    console.log("hashedPassword:", hashedPassword);
+    const insertValues = {
+      email: email,
+      password: hashedPassword,
+    };
+    console.log("insertValues:", insertValues);
+    const newUser = await db.insert(usersTable).values(insertValues);
+
+    res.status(201).send({
+      message: "user created successfully",
+      newUser,
+    });
+  } catch (error) {
+    console.error("error:", error);
+    res.status(500).send({
+      message: ("Error creating user:", error),
+      error,
+    });
+  }
+});
 
 module.exports = app;
--- src/db/schema.js
+++ src/db/schema.js
@@ -1,6 +1,22 @@
-import { pgTable, varchar, uuid } from "drizzle-orm/pg-core";
-export const usersTable = pgTable("users", {
-    id: uuid().primaryKey().defaultRandom().notNull().unique(),
-    email: varchar({ length: 255 }).notNull().unique(),
-});
-//# sourceMappingURL=schema.js.map
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.usersTable = void 0;
+var pg_core_1 = require("drizzle-orm/pg-core");
+var defaultTableProperties = {
+    id: (0, pg_core_1.uuid)().primaryKey().defaultRandom().notNull().unique(),
+    createdAt: (0, pg_core_1.timestamp)().defaultNow().notNull(),
+    updatedAt: (0, pg_core_1.timestamp)().defaultNow().notNull(),
+    deletedAt: (0, pg_core_1.timestamp)()
+};
+exports.usersTable = (0, pg_core_1.pgTable)("users", __assign(__assign({}, defaultTableProperties), { email: (0, pg_core_1.varchar)({ length: 255 }).notNull().unique(), password: (0, pg_core_1.text)().notNull() }));
