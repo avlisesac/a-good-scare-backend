ca1275df2a698dc1e1485f19688aee50ad073fea
avlisesac <casejsilva@gmail.com>
2025-12-11 21:01:31 -0500

    initializing login route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -22,11 +23,12 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api", indexRouter);
 app.use("/api/users", usersRouter);
 
+// TODO: Move to route file
 app.post("/api/register", async (req, res, next) => {
   console.log("req.body:", req.body);
   try {
     const { email, password } = req.body;
-    const missingRequiredParametersMessage = "Missing required parameters:";
+    let missingRequiredParametersMessage = "Missing required parameters: ";
     const missingRequiredParameters = [];
     if (!email) {
       missingRequiredParameters.push("email");
@@ -35,8 +37,8 @@ app.post("/api/register", async (req, res, next) => {
       missingRequiredParameters.push("password");
     }
     if (missingRequiredParameters.length > 0) {
-      missingRequiredParametersMessage +=
-        missingRequiredParameters.concat(", ");
+      console.log("missingRequiredParameters:", missingRequiredParameters);
+      missingRequiredParametersMessage += missingRequiredParameters.join(", ");
       throw missingRequiredParametersMessage;
     }
 
@@ -68,6 +70,65 @@ app.post("/api/register", async (req, res, next) => {
     console.error("error:", error);
     res.status(500).send({
       message: ("Error creating user:", error),
+      error,
+    });
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  console.log("req.body:", req.body);
+  try {
+    const { email, password } = req.body;
+    let missingRequiredParametersMessage = "Missing required parameters: ";
+    const missingRequiredParameters = [];
+    if (!email) {
+      missingRequiredParameters.push("email");
+    }
+    if (!password) {
+      missingRequiredParameters.push("password");
+    }
+    if (missingRequiredParameters.length > 0) {
+      console.log("missingRequiredParameters:", missingRequiredParameters);
+      missingRequiredParametersMessage += missingRequiredParameters.join(", ");
+      throw missingRequiredParametersMessage;
+    }
+
+    // Check for existing user w/ email.
+    const existingUsersWithEmail = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.email, email));
+    console.log("existingUsersWithEmail:", existingUsersWithEmail);
+
+    if (!existingUsersWithEmail || existingUsersWithEmail.length < 1) {
+      throw "No user found with this email address.";
+    }
+    const targetUser = existingUsersWithEmail[0];
+
+    const passwordsMatch = await bcrypt.compare(password, targetUser.password);
+
+    if (!passwordsMatch) {
+      throw "Password is incorrect.";
+    }
+
+    const token = jwt.sign(
+      {
+        userId: targetUser.id,
+        userEmail: targetUser.email,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: process.env.JWT_EXPIRATION_TIME }
+    );
+
+    res.status(200).send({
+      message: "Login Successful",
+      email: targetUser.email,
+      token,
+    });
+  } catch (error) {
+    console.error("error:", error);
+    res.status(400).send({
+      message: ("Login failed", error),
       error,
     });
   }
