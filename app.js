c8a43524e7595e01ba5b527dfe770bba1e5bf960
avlisesac <casejsilva@gmail.com>
2026-01-24 12:02:34 -0500

    more ios cors changes

--- app.js
+++ app.js
@@ -29,8 +29,11 @@ app.use(
   cors({
     origin: allowedOrigins,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+app.options("*", cors());
 
 app.set("trust proxy", 1);
 
